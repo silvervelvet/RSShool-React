9fc80ca464a74f687bc14d6064896474d07ab147
silvervelvet <108361662+silvervelvet@users.noreply.github.com>
2024-02-28 00:40:54 +0300

    fix: fix husky

--- src/App.tsx
+++ src/App.tsx
@@ -5,15 +5,14 @@ import './App.css';
 
 function App() {
   const [count, setCount] = useState(0);
-  const a;
 
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
+        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
-        <a href="https://react.dev" target="_blank">
+        <a href="https://react.dev" target="_blank" rel="noreferrer">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
